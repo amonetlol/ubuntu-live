035b2fcaf662360a3308598ac7fb1a6e943af138
amonetlol <65382640+amonetlol@users.noreply.github.com>
2024-08-31 17:54:01 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 #sudo apt update
 
 # Instalar os pacotes necessários
-sudo apt install -y open-vm-tools-desktop gnome-shell-extension-manager curl htop wget
+sudo apt install -y open-vm-tools-desktop gnome-shell-extension-manager curl htop wget libcanberra-gtk-module
 
 # Instalar extensões no GNOME Extension Manager
 gnome-extensions install ip-finder@extensions.gnome.org
@@ -13,9 +13,8 @@ gnome-extensions install vitals@extensions.gnome.org
 # Pre Install
 mkdir ~/.src
 
-
 # Download uFetch
-wget https://gitlab.com/jschx/ufetch/-/raw/main/ufetch-ubuntu?ref_type=heads
+git clone https://gitlab.com/jschx/ufetch ~/.src/ufetch
 
 # Dar permissão de execução ao ufetch-ubuntu
 chmod +x ~/.src/ufetch/ufetch-ubuntu
@@ -24,11 +23,11 @@ chmod +x ~/.src/ufetch/ufetch-ubuntu
 echo "alias ft='sh ~/.src/ufetch/ufetch-ubuntu'" >> ~/.bashrc
 source ~/.bashrc
 
-# Instalar o uBlock Origin no Firefox
-firefox https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi
-
 # Executar o script do Firefox-UI-Fix
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/black7375/Firefox-UI-Fix/master/install.sh)"
+
+# Instalar o uBlock Origin no Firefox
+firefox https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi
 
 # Aplicar o tema Spaceship para o Firefox
 firefox https://addons.mozilla.org/firefox/addon/spaceship_theme/
