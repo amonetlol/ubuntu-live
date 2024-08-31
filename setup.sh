54858e709c555a69a6743f673ca19af744222fac
amonetlol <65382640+amonetlol@users.noreply.github.com>
2024-08-31 17:44:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,21 @@
 #sudo apt update
 
 # Instalar os pacotes necessários
-sudo apt install -y open-vm-tools-desktop gnome-shell-extension-manager curl
+sudo apt install -y open-vm-tools-desktop gnome-shell-extension-manager curl htop wget
 
 # Instalar extensões no GNOME Extension Manager
 gnome-extensions install ip-finder@extensions.gnome.org
 gnome-extensions install vitals@extensions.gnome.org
+
+# Download uFetch
+wget https://gitlab.com/jschx/ufetch/-/raw/main/ufetch-ubuntu?ref_type=heads
+
+# Dar permissão de execução ao ufetch-ubuntu
+chmod +x ~/ufetch-ubuntu
+
+# Adicionar alias ao bash
+echo "alias ft='sh ~/ufetch-ubuntu'" >> ~/.bashrc
+source ~/.bashrc
 
 # Instalar o uBlock Origin no Firefox
 firefox https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi
