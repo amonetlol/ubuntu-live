535e81ecbe13ccc02dd5a2f6a6460c09647ab4c2
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-06-20 22:30:20 -0300

    Create hidden_shortcut.sh

---
+++ hidden_shortcut.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+apps=("btop"
+      "htop"
+      "avahi-discover"
+      "picom"
+      "nvim"
+      "Alacritty"
+      "rofi-theme-selector"
+      "bvnc"
+      "bssh"
+      "arandr"
+      "vim"
+      "ranger"
+      "kitty"
+      "kvantummanager"
+      "meld"
+      "qt5ct"
+      "qt6ct"
+      "qv4l2"
+      "qvidcap"
+      "nvim"
+      "stoken-gui"
+      "stoken-gui-small"
+      "tint2"
+      "yad-settings"
+      )
+
+mkdir -p ~/.local/share/applications/
+for app in "${apps[@]}"; do
+  if [ -f "/usr/share/applications/$app.desktop" ]; then
+    cp "/usr/share/applications/$app.desktop" ~/.local/share/applications/
+    echo "NoDisplay=true" >> ~/.local/share/applications/$app.desktop
+    echo "Ocultado: $app"
+  else
+    echo "Arquivo $app.desktop não encontrado"
+  fi
+done
